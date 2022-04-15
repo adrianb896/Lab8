8299859ca08ed98b6d25620c0204addc2b24a9f8
adrianb896 <65195311+adrianb896@users.noreply.github.com>
2022-04-14 22:32:41 -0700

    Add files via upload

---
+++ Lab8/src/hazardControl/Employee.java
@@ -0,0 +1,26 @@
+package hazardControl;
+
+public abstract class Employee implements HReporter_IF {
+    protected String name;
+    protected Administrator overseer;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public abstract void seeDanger(HReporter_IF r, Hazard hazard);
+
+    public abstract void evacuate();
+
+    public void setDirectOverseer(HReporter_IF directOverseer) {
+        this.overseer = (Administrator) directOverseer;
+    }
+
+    public HReporter_IF getDirectOverseer() {
+        return overseer;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
