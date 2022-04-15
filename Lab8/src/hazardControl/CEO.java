0f49d897722bc9def3115b2fa429e180089eceb2
adrianb896 <65195311+adrianb896@users.noreply.github.com>
2022-04-14 22:10:07 -0700

    Update CEO.java

--- Lab8/src/hazardControl/CEO.java
+++ Lab8/src/hazardControl/CEO.java
@@ -1,24 +1,45 @@
 package hazardControl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CEO {
-    public class implementDecision<d>(List<Decision> ds) {
-    private void sortByPriority(ds) {
+
+    public CEO(String name) {
+        super(name);
+}
+
+    @Override
+    public void seeDanger(HReporter_IF r, Hazard h) {
+        List<Decision> decisions = new ArrayList<>();
+        for (Employee admin: members) {
+            if(admin instanceof Manager)
+                decisions.addAll(((Manager)admin).suggestDecisions(hazard));
+        }
+        implementDecision(decisions);
+    }
+    private List<Decision> sortByPriority(List<Decision> ds){
+        List<Decision> decisions = new ArrayList<>();
+        while(!ds.isEmpty()){
+            Decision topPriority = ds.remove(0);
+            for (Decision d: ds){
+                if(d.getPriority() > topPriority.getPriority())
+                    topPriority = d;
+            }
+            decisions.add(topPriority);
+        }
+        return decisions;
+    }
+
+
+    public void implementDecision(List<Decision> ds){
         ds = sortByPriority(ds);
+        int dsToBeExecuted = 2;
+        while (!ds.isEmpty() && dsToBeExecuted-- > 0)
+            ds.remove(0).execute(this);
     }
-    Decision d = ds.get(0);
-         = ds.get(1);
-        d.execute(this);
-}
 
-
-    public class seeDanger(r:HReporter_IF; h:Hazard) {
-        List<Decision> decisions = new List<Decision>;
-    for(each e:members) {
-            Manager m = (Manager) e;
-            decisions.append(m.suggestDecision(h))};
+    public void report(){
+        System.out.println("The city’s environmental department is notified");
     }
-    implementDecision(decisions);
 }
-
