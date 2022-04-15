19e27f115c4d28516538ff7ef6eb26911681e558
adrianb896 <65195311+adrianb896@users.noreply.github.com>
2022-04-14 23:35:17 -0700

    Delete HReporter_IF.java

--- Lab8/src/hazardControl/HReporter_IF.java
+++
@@ -1,9 +0,0 @@
-package src.Personel;
-
-import src.Actions.Hazard;
-
-public interface HReporter_IF {
-    public void seeDanger(HReporter_IF reporter, Hazard hazard);
-    public void setDirectOverseer(HReporter_IF directOverSeer);
-    public HReporter_IF getDirectOverseer();
-}
