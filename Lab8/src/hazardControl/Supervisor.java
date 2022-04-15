96efc2225bc944de761229a5efff09c798dbd3b8
adrianb896 <65195311+adrianb896@users.noreply.github.com>
2022-04-14 23:35:48 -0700

    Delete Supervisor.java

--- Lab8/src/hazardControl/Supervisor.java
+++
@@ -1,9 +0,0 @@
-package src.Personel;
-
-public class Supervisor extends DirectAdministrator{
-
-    //this is the constructor and the parameter here is name
-    public Supervisor(String name) {
-        super(name);
-    }
-}
