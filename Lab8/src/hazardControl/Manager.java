63a8791009e6d98846c594280d085d0dbef9638a
JoseGonzalez08 <42353875+JoseGonzalez08@users.noreply.github.com>
2022-04-14 20:21:44 -0700

    testing

--- Lab8/src/hazardControl/Manager.java
+++ Lab8/src/hazardControl/Manager.java
@@ -26,6 +26,7 @@ public class Manager {
         Boolean reportToCEO = true;
         for (each e: members) {
             DirectAdministrator d = (DirectAdministrator.e);
+            //hello world
 
         }
     }
