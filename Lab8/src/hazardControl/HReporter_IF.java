f579cfc522bb6f622ca122845bc9e47d3da39d16
adrianb896 <65195311+adrianb896@users.noreply.github.com>
2022-04-14 22:51:52 -0700

    Update HReporter_IF.java

--- Lab8/src/hazardControl/HReporter_IF.java
+++ Lab8/src/hazardControl/HReporter_IF.java
@@ -2,8 +2,8 @@ package src.Personel;
 
 import src.Actions.Hazard;
 
-public interface IReporterHazard {
-    public void seeDanger(IReporterHazard reporter, Hazard hazard);
-    public void setDirectOverseer(IReporterHazard director);
-    public IReporterHazard getDirectOverseer();
+public interface HReporter_IF {
+    public void seeDanger(HReporter_IF reporter, Hazard hazard);
+    public void setDirectOverseer(HReporter_IF directOverSeer);
+    public HReporter_IF getDirectOverseer();
 }
